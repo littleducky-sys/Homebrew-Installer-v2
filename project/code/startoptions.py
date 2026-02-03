b8a7bc8d9ac36b7f425e815939dc3bd2341fbd5c
littleducky-sys <ethankllret@gmail.com>
2026-02-03 18:38:50 -0500

    small update

--- project/code/startoptions.py
+++ project/code/startoptions.py
@@ -1,3 +1,28 @@
+mode = ""
+path = ""
+
+option = input("Where this program's folder being stored (ex C:/Users/joe/Downloads/HBIV2)?")
+
+path = option
+
+print(f"Program folder set to: {path}")
+print("if this is the wrong install folder, please restart the program right now.")
+
+print("(WINDOWS ONLY PROGRAM)")
 print("Welcome to Homebrew Installer v2!")
+print()
+
 print("y=Console/n=GUI")
 option = input("Start program in console or (unstable) GUI mode (y/n)?")
+if option.lower() == 'y':
+    print("Starting in console mode...")
+    mode = "console"
+elif option.lower() == 'n':
+    print("Starting in GUI mode...")
+    mode = "gui"
+else:
+    print("Invalid option. Starting in console mode by default.")
+    mode = "console"
+
+print(f"Mode set to: {mode}")
+print("Settings.json updated.")
