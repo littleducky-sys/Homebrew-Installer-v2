5c81f811f39fdc7d78f19b0f9f56e0497a34d214
littleducky-sys <ethankllret@gmail.com>
2026-02-03 20:23:35 -0500

    SMALLER update

--- project/code/startoptions.py
+++ project/code/startoptions.py
@@ -1,28 +1,38 @@
+import json
+import os
+
 mode = ""
-path = ""
 
-option = input("Where this program's folder being stored (ex C:/Users/joe/Downloads/HBIV2)?")
+SETTINGS_PATH = "project/meta/settings.json"
 
-path = option
-
-print(f"Program folder set to: {path}")
-print("if this is the wrong install folder, please restart the program right now.")
-
-print("(WINDOWS ONLY PROGRAM)")
-print("Welcome to Homebrew Installer v2!")
-print()
-
-print("y=Console/n=GUI")
-option = input("Start program in console or (unstable) GUI mode (y/n)?")
-if option.lower() == 'y':
-    print("Starting in console mode...")
-    mode = "console"
-elif option.lower() == 'n':
-    print("Starting in GUI mode...")
-    mode = "gui"
+if os.path.exists(SETTINGS_PATH):
+    with open(SETTINGS_PATH, "r") as file:
+        settings = json.load(file)
 else:
-    print("Invalid option. Starting in console mode by default.")
-    mode = "console"
+    settings = {}
 
-print(f"Mode set to: {mode}")
-print("Settings.json updated.")
+if not settings.get("cgui"):
+    print("Welcome to Homebrew Installer v2!")
+
+    print("y=Console/n=GUI")
+    option = input("Start program in console or (unstable) GUI mode (y/n)? ")
+
+    if option.lower() == 'y':
+        print("Starting in console mode...")
+        mode = "console"
+    elif option.lower() == 'n':
+        print("Starting in GUI mode...")
+        mode = "gui"
+    else:
+        print("Invalid option. Starting in console mode by default.")
+        mode = "console"
+
+    settings["cgui"] = mode
+
+    print(f"Mode set to: {mode}")
+    print("Settings.json updated.")
+
+    with open(SETTINGS_PATH, "w") as file:
+        json.dump(settings, file, indent=4)
+
+print("test")
