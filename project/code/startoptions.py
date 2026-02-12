c8b34ce5e3c59b4e9261af392919c136fba3757e
littleducky-sys <ethankllret@gmail.com>
2026-02-12 15:46:37 -0500

    Update

--- project/code/startoptions.py
+++ project/code/startoptions.py
@@ -1,12 +1,25 @@
 import json
 import os
 import consoleselect
+import sys
+import platform
+from time import sleep
+
 
 mode = ""
 
 print("Welcome to Homebrew Installer v2!")
-print("This program is meant for windows.")
+print("This build of the program is meant for windows.")
+print("other builds for linux/macOS should be available,")
+print("at the same website you got this windows build from.")
 
+if platform.system() != "Windows":
+   print("OS FOUND "+str(platform.system())+" PLEASE READ MESSAGE ABOVE THIS")
+   print("EXITING PROGRAM IN 10 SECONDS....")
+   sleep(10)
+   sys.exit("OS NOT ALLOWED")
+
+#this should be the only hardcoded path but i am not sure.
 appjson_path = "project/meta/app.json"
 
 with open(appjson_path, "r") as f:
